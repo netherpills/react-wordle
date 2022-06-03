abf7ad3d706aedacd7222d1ae74240b2e79fb8e5
netherpills <42825176+netherpills@users.noreply.github.com>
2022-06-03 00:07:07 -0300

    Update InfoModal.tsx
    info

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,63 +8,64 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="¿Cómo jugar?" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Tenés que adivina la palabra de 5 letras en 6 intentos.
+        Cada vez que probás una palabra diferente, el color de los espacios
+        ira cambiando acorde a si la letra está en la palabra o no.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="J"
           status="correct"
         />
-        <Cell value="E" />
-        <Cell value="A" />
+        <Cell value="O" />
         <Cell value="R" />
+        <Cell value="G" />
         <Cell value="Y" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        La letra J está en la palabra y en el lugar correcto.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="S" />
+        <Cell value="A" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="R"
           status="present"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="N" />
+        <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        La letra N está en la palabra pero no en el lugar correcto.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
+        <Cell value="C" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="R" />
+        <Cell isRevealing={true} isCompleted={true} value="T" status="absent" />
+        <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        La letra T no se encuentra en ningún lugar de la palabra.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        El codigo original es de
+        >> -{' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
+          cwackerfuss
         </a>{' '}
       </p>
     </BaseModal>
