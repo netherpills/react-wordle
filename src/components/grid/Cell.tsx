bea6e2ef538a7004ec56518caf595f95a6411570
netherpills <42825176+netherpills@users.noreply.github.com>
2022-06-02 23:59:16 -0300

    Update Cell.tsx

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -26,7 +26,7 @@ export const Cell = ({
   const classes = classnames(
     'w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white',
     {
-      'bg-indigo-300 dark:bg-indigo-900 border-slate-200 dark:border-slate-600':
+      'bg-indigo-100 dark:bg-indigo-900 border-slate-200 dark:border-slate-600':
         !status,
       'border-black dark:border-slate-100': value && !status,
       'absent shadowed bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700':
