dbb9d24955a6ef67bb76e6f5a6ed742785e33534
netherpills <42825176+netherpills@users.noreply.github.com>
2022-06-02 23:23:44 -0300

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,29 +1,29 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['nashe', 'buenardo', 'god']
+export const WIN_MESSAGES = ['nashe', 'buenardo', 'god', 'de ruta', 'de rutherford', 'ido']
 export const GAME_COPIED_MESSAGE = 'Partida copiada al portapapeles'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Faltan letras'
 export const WORD_NOT_FOUND_MESSAGE = 'Palabra no encontrada'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+  'El modo dificil solo se puede habilitar al principio'
 export const HARD_MODE_DESCRIPTION =
-  'Any revealed hints must be used in subsequent guesses'
+  'Todas las pistas reveladas deberán ser usadas en los siguientes intentos'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Para cambiar de color los espacios'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
   `La palabra era ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
+  `Tenés que usar ${guess} en la posición ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
+  `El acierto debe contener ${letter}`
 export const ENTER_TEXT = 'Probar'
 export const DELETE_TEXT = 'Borrar'
 export const STATISTICS_TITLE = 'Estadísticas'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
+export const GUESS_DISTRIBUTION_TEXT = 'Distribución de Aciertos'
+export const NEW_WORD_TEXT = 'Nueva palabra en'
 export const SHARE_TEXT = 'Compartir'
 export const TOTAL_TRIES_TEXT = 'Total de intentos'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+export const SUCCESS_RATE_TEXT = 'Tasa de éxito'
+export const CURRENT_STREAK_TEXT = 'Racha actual'
+export const BEST_STREAK_TEXT = 'Mejor racha'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
   "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
