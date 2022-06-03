eec60da23e477611812f908c952868b0bf96091e
netherpills <42825176+netherpills@users.noreply.github.com>
2022-06-02 23:40:50 -0300

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import {
   InformationCircleIcon,
 } from '@heroicons/react/outline'
 import { GAME_TITLE } from '../../constants/strings'
+import CslHeader from '../../assets/csl_header.png'
 
 type Props = {
   setIsInfoModalOpen: (value: boolean) => void
@@ -23,7 +24,7 @@ export const Navbar = ({
           className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
           onClick={() => setIsInfoModalOpen(true)}
         />
-        <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>
+        <p className="text-xl ml-2.5 font-bold dark:text-white">src={CslHeader}</p>
         <div className="right-icons">
           <ChartBarIcon
             className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
