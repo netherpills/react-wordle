e1bde76cfa3a79cfe1b82bfb8a49903ca3994f54
netherpills <42825176+netherpills@users.noreply.github.com>
2022-06-06 19:17:23 -0300

    Update comment.php

--- public/comment.php
+++ public/comment.php
@@ -3,19 +3,18 @@ mysql_connect("localhost","root","");
 mysql_select_db("cmtdb");
 error_reporting(E_ALL ^ E_NOTICE);
 $notify = "";
-$name=$_POST['name'];
-$comment=$_POST['comment'];
-$submit=$_POST['submit'];
+$comment=$_POST['sugerir'];
+$submit=$_POST['enviar'];
 if(isset($_POST['notify_box'])){ $notify = $_POST['notify_box']; }
 $dbLink = mysql_connect("localhost", "root", "");
     mysql_query("SET character_set_client=utf8", $dbLink);
     mysql_query("SET character_set_connection=utf8", $dbLink);
  
-if($submit)
+if($enviar)
 {
-    if($name&&$comment)
+    if($sugerir)
     {
-        $insert=mysql_query("INSERT INTO comment (name,comment) VALUES ('$name','$comment') ");
+        $insert=mysql_query("INSERT INTO sugerir (sugerir) VALUES ('$sugerir') ");
     }
     else
     {
@@ -28,6 +27,6 @@ mysql_query("SET character_set_results=utf8", $dbLink);
 mb_language('uni');
 mb_internal_encoding('UTF-8');
  
-$sql = "SELECT * FROM comment";
+$sql = "SELECT * FROM sugerir";
 $getquery = mysql_query($sql);
 ?>
